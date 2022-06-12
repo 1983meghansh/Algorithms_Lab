a070fa033c1ab93e3a2d664b5fb5673a2198c924
1983meghansh <79457667+1983meghansh@users.noreply.github.com>
2022-06-12 23:58:57 +0530

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,75 +1,101 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-// int count=0;
-void merge(int *arr,int l,int mid,int r)
+int counter = 0;
+
+void merge(int array[], int left, int mid, int right)
 {
-    int i=l;
-    int j=mid+1;
-    int k=l;
-    int b[r-l+1];
-    
-    while(i<=mid and j<=r)
+    int i = 0, j = 0, k = left;
+    int an = mid - left + 1;
+    int bn = right - mid;
+    int a[an], b[bn];
+    for (int i = 0; i < an; i++)
     {
-        if(arr[i]<arr[j])
+        a[i] = array[left + i];
+        // cout<<a[i]<<" ";
+    }
+    // cout<<endl;
+    for (int i = 0; i < bn; i++)
+    {
+        b[i] = array[mid + 1 + i];
+        // cout<<b[i]<<" ";
+    }
+    // cout<<endl;
+    i = 0;
+    j = 0;
+
+    while (i < an and j < bn)
+    {
+        counter++;
+        if (a[i] > b[j])
         {
-            b[k]=arr[i];
-            i++;
-        }
-        else{
-            b[k]=arr[j];
+            array[k] = b[j];
             j++;
+        }
+        else
+        {
+            array[k] = a[i];
+            i++;
         }
         k++;
-        // count++;
     }
-    if(j>r)
+    while (i < an)
     {
-        while(i<=mid)
-        {
-            b[k]=arr[i];
-            i++;
-            k++;
-            // count++;
-        }
+        counter++;
+        array[k] = a[i];
+        k++;
+        i++;
     }
-    else{
-        while(j<=r)
-        {
-            b[k]=arr[j];
-            j++;
-            k++;
-            // count++;
-        }
-    }
-    for(int i=l;i<=r;i++)
+    while (j < bn)
     {
-        arr[i]=b[i];
+        counter++;
+        array[k] = b[j];
+        k++;
+        j++;
     }
 }
 
-void merge_sort(int *arr,int l,int r)
+void mergeSort(int array[], int start, int end)
 {
-    if(l<r)
-    {
-        int mid=(l+r)/2;
-        merge_sort(arr,l,mid);
-        merge_sort(arr,mid+1,r);
-        merge(arr,l,mid,r);
-    }
+    if (start >= end)
+        return;
+    int mid = (end - start) / 2 + start;
+    mergeSort(array, start, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, start, mid, end);
 }
 
-int main(){
-    int n;
-    cout<<"enter number of elements"<<"\n";
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++)
+int main()
+{
+    int arr[] = {5, 4, 3, 2, 1};
+    // int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    mergeSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cout << arr[i] << " ";
     }
-    merge_sort(arr,0,n-1);
-    for(int i=0;i<n;i++)
-    {
-        cout<<arr[i]<<" ";
+    cout << endl<<"The count of the basic operations executed for this input is : ";
+    cout << counter << endl;
+
+    int a[500], b[500], c[500];
+    for(int i = 0; i < 500; i ++){
+        a[i] = i;
+        b[i] = 500 - i;
+        c[i] = int(rand()) % (i+1);
     }
+    cout<<"INDEX\t BEST\t WORST \t RANDOM\n";
+    for(int i = 16; i < 50; i ++){
+        counter = 0;
+        mergeSort(a, 0, i);
+        cout<<"  "<<i<<"\t "<<counter<<"  \t";
+        counter = 0;
+        mergeSort(b, 0, i);
+        cout<<counter<<" \t  ";
+        counter = 0;
+        mergeSort(c, 0, i);
+        cout<<counter<<" \n";
+    }
+
+    cout << endl;
+    return 0;
 }
